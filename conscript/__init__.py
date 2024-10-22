c17a76861e80ee9b003ce15e59d38052bfc6d5d4
John Sirois <john.sirois@gmail.com>
2024-10-21 23:16:02 -0700

    Add support for PI.

--- conscript/__init__.py
+++ conscript/__init__.py
@@ -1,8 +1,3 @@
-"""Conscript console scripts in your own Swiss Army Knife.
-
-A Python BusyBox console script.
-"""  # N.B.: Flit uses this as our distribution description.
-
 from __future__ import absolute_import
 
-__version__ = "0.1.7"  # N.B.: Flit uses this as out distribution version.
+__version__ = "0.1.7"  # N.B.: Setuptools is configured to use this as our distribution version.
