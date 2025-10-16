33387d9e0d0eb8c5b52f88aff39d4f671b71866e
John Sirois <john.sirois@gmail.com>
2025-10-16 15:38:32 -0700

    Prepare the 0.1.9 release. (#13)

--- conscript/__init__.py
+++ conscript/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
 
-__version__ = "0.1.8"  # N.B.: Setuptools is configured to use this as our distribution version.
+__version__ = "0.1.9"  # N.B.: Setuptools is configured to use this as our distribution version.
